61fdea3874863a56bedc755b77b9490807b11c20
Menachem Farkash <menachem.farkash1@gmail.com>
2025-02-10 06:44:05 +0200

    Basic navmesh agents
    While the game is running, press F to make the pedestrians move to their target location

--- Street Game/Assets/_Scripts/Pedestrians/WalkingEngine.cs
+++ Street Game/Assets/_Scripts/Pedestrians/WalkingEngine.cs
@@ -4,17 +4,19 @@ using UnityEngine.AI;
 public class WalkingEngine : MonoBehaviour {
 
     public NavMeshAgent agent;
+    public Transform startLocation;
+    public Transform endLocation;
 
-    void Start() {
 
-    }
 
     void Update() {
-
+        if (Input.GetKeyDown(KeyCode.F)) {
+            SetPlayerDestination(endLocation.position);
+        }
     }
 
     public void SetPlayerDestination(Vector3 targetPosition) {
-        if (agent != null) {
+        if (agent == null) {
             print("No Agent Attached");
             return;
         }
